8feff9a0e5da908a485e2b7e97975a3424e0da0b
agentg2007 <allen_gepulla@yahoo.com>
2022-03-25 09:38:16 +0800

    version 1.0.0

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+export declare const register: (name: string, action: () => any) => {
+    register: any;
+};
+declare const _default: (context: any, str: string) => string;
+export default _default;
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import _, { Dictionary } from "lodash";
+
+const Funcs: Dictionary<() => any> = {};
+const EmptyFunc = () => "";
+export const register = (name: string, action: () => any) => {
+    Funcs[name] = action;
+    return { register };
+}
+export default (context: any, str: string) => {
+    const rx = /\${(.*?)\}/gm;
+    const keys = []
+    let m: any;
+    do {
+        m = rx.exec(str);
+        m != null && keys.push({
+            Full: m[0],
+            Data: m[1]
+        })
+    } while (m)
+    let result = str;
+    keys.forEach(({ Full, Data }) => {
+        if (_.startsWith(Data, "Func:")) {
+            const name = _.split(Data, ':')[1]
+            result = result.replace(Full, (Funcs[name] ?? EmptyFunc)());
+        } else {
+            result = result.replace(Full, _.at(context, [Data])[0] as any ?? "");
+        }
+    });
+    return result;
+}
