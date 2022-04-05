af49ec834499c813001678e63a4fc90e93fd1c9c
agentg2007 <allen_gepulla@yahoo.com>
2022-04-05 20:17:14 +0800

    Deleted dist

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const register: (name: string, action: () => any) => {
-    register: any;
-};
-declare const _default: (context: any, str: string) => string;
-export default _default;
-//# sourceMappingURL=index.d.ts.map
